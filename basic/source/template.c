fdc38d18c346497576ddf26f8858ba7337c7e6ae
Sahil Makim <makim.sahil@gmail.com>
2025-09-14 21:28:31 -0400

    Fix include path issue for nds.h reference

---
+++ basic/source/template.c
@@ -0,0 +1,12 @@
+// Sahil Makim Basic Project
+#include <nds.h>
+
+
+int main(void) {
+	u16* mainGFX = oamAllocateGfx(&oamMain, SpriteSize_16x16, SpriteColorFormat_Bmp); // top screen square
+	u16* subGFX = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_Bmp); // bottom screen square
+
+}
+
+void createSquare() {	
+}
